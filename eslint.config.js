c36f3706d4382ff7b5e79c6511955275635a6159
Sangjin Kim <rla2892@naver.com>
2024-08-25 12:39:21 +0900

    Modify lint for quatation. Allow backtick.

--- eslint.config.js
+++ eslint.config.js
@@ -17,9 +17,8 @@ module.exports = [
       "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
     },
     rules: {
-      // 여기에 프로젝트에 필요한 규칙을 추가하세요
       "semi": ["error", "always"],
-      "quotes": ["error", "single"],
+      "quotes": ["error", "single", { "allowTemplateLiterals": true }],
       "@typescript-eslint/no-unused-vars": ["error"],
     },
   },
