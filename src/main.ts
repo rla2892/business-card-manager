3086a1be570e8d9b76a1775dbf4cb2bf2dcb7796
Sangjin Kim <rla2892@naver.com>
2024-08-25 11:58:10 +0900

    Implement console question

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('Hello World');
+import * as readline from 'readline';
+
+// readline 인터페이스 생성
+const consoleReader = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+function startQuestion() {
+	consoleReader.question(`Press 'q' to exit. \nInput : `, (keyInput) => {
+		// If the key is 'q', exit the program
+		keyInput = keyInput.toLowerCase();
+		if (keyInput === 'q') {
+			console.log(`Exiting the program...`);
+			consoleReader.close();
+		} else {
+			startQuestion();
+		}
+
+	});
+} 
+
+startQuestion();
