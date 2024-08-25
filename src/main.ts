d86c562a33fdb56dd414d9c6a1c7e3eec3960631
Sangjin Kim <rla2892@naver.com>
2024-08-25 12:09:13 +0900

    Move comments

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ const consoleReader = readline.createInterface({
 
 function startQuestion() {
 	consoleReader.question(`Press 'q' to exit. \nInput : `, (keyInput) => {
-		// If the key is 'q', exit the program
 		keyInput = keyInput.toLowerCase();
 		if (keyInput === 'q') {
+			// If the key is 'q', exit the program
 			console.log(`Exiting the program...`);
 			consoleReader.close();
 		} else {
